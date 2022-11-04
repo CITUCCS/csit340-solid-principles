b18b5c20b2f147143b08aaac2a389d16c5aabe26
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-11-04 23:22:33 +0800

    make main top-level

--- interface-segregation-principle/ISP/Program.cs
+++ interface-segregation-principle/ISP/Program.cs
@@ -1,21 +1,14 @@
-﻿namespace ISP
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ILandVehicle car = new Car();
-            car.Drive();
-            // car.Fly(); <--- now impossible
+﻿using ISP;
 
-            IAirVehicle airplane = new Airplane();
-            IAirVehicle jet = new Jet();
-            airplane.Fly();
-            jet.Fly();
+ILandVehicle car = new Car();
+car.Drive();
+// car.Fly(); <--- now impossible
 
-            IHybridVehicle vehicle = new MultiFunctionalCar(car, jet);
-            vehicle.Fly();
-            vehicle.Drive();
-        }
-    }
-}
+IAirVehicle airplane = new Airplane();
+IAirVehicle jet = new Jet();
+airplane.Fly();
+jet.Fly();
+
+IHybridVehicle vehicle = new MultiFunctionalCar(car, jet);
+vehicle.Fly();
+vehicle.Drive();
