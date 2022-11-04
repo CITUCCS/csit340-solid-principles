1e6615ba6edb3fd1f4a7219bf92abbcfca80b7c6
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-11-04 23:15:30 +0800

    Add DIP

---
+++ dependency-inversion-principle/DIP/EmployeeStatistics.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace DIP
+{
+    public class EmployeeStatistics
+    {
+        private readonly IEmployeeSearchable _empManager;
+
+        public EmployeeStatistics(IEmployeeSearchable employeeManager)
+        {
+            _empManager = employeeManager;
+        }
+
+        public int CountFemaleManagers()
+        {
+            return _empManager.GetEmployeesByGenderAndPosition(Gender.Female, Position.Manager).Count();
+        }
+
+        public int CountMaleManagers()
+        {
+            return _empManager.GetEmployeesByGenderAndPosition(Gender.Male, Position.Manager).Count();
+        }
+
+        public int CountFemaleAdministors()
+        {
+            return _empManager.GetEmployeesByGenderAndPosition(Gender.Female, Position.Administrator).Count();
+        }
+    }
+}
---
+++ dependency-inversion-principle/DIP/Program.cs
@@ -0,0 +1,9 @@
+﻿using DIP;
+using System;
+
+var empManager = new EmployeeManager();
+empManager.AddEmployee(new Employee { Name = "Leen", Gender = Gender.Female, Position = Position.Manager });
+empManager.AddEmployee(new Employee { Name = "Mike", Gender = Gender.Male, Position = Position.Administrator });
+
+var stats = new EmployeeStatistics(empManager);
+Console.WriteLine($"Number of female managers in our company is: {stats.CountFemaleManagers()}");
